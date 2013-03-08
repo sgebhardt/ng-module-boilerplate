e8a02197b7250110b240b02fd913abd124d1e6d7
Sebastian Gebhardt <info@sebastian-gebhardt.info>
2013-03-08 18:55:51 +0100

    add MyApp module

--- js/app.js
+++ js/app.js
@@ -2,5 +2,11 @@
 
 /* App Module */
 		
-
-
+angular.module('MyApp', [])
+	.config( function myAppConfig ( $routeProvider ) {
+		$routeProvider.otherwise({ redirectTo: '/home' });
+	})
+		
+	.controller('AppController', [ '$scope', '$location', function AppCtrl ( $scope, $location ) {
+		$scope.Hello = "Hello, World!";
+	}]);
