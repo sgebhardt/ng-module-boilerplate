97dc6ca952f33e78379ae3de07bfefeea59b4b4d
Sebastian Gebhardt <info@sebastian-gebhardt.info>
2013-03-08 19:49:42 +0100

    add blogentry comments

--- js/blog_entries.js
+++ js/blog_entries.js
@@ -6,7 +6,17 @@ angular.module('MyApp.blog.entries', [])
 	
 .controller('BlogEntriesController', [ '$scope', function BlogEntriesController( $scope ) {
 	$scope.Entries = [	
-		{ text: "this is the first blog entry" },
-		{ text: "this is the second blog entry" }
+		{ text: "this is the first blog entry", 
+		  comments: [
+			{text: "1. Comment"}, 
+			{text: "2. Comment"}
+		  ] 
+	    },
+		{ text: "this is the second blog entry" , 
+		  comments: [
+			{text: "3. Comment"}, 
+			{text: "4. Comment"}
+		  ] 
+		}
 	];
 }]);
