a9dd5c5ce0d95ddb888679fbda7d539a1d2204e3
Sebastian Gebhardt <sebastian.gebhardt@d-fine.de>
2013-03-10 00:43:51 +0100

    add tests for AppController and BlogController

---
+++ test/unit/controllersSpec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+describe('MyApp controllers', function() {
+
+  	describe('AppController', function(){
+  		var scope, ctrl, $httpBackend;
+  		
+  		beforeEach(module('MyApp'));
+  	
+	  	beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+	      $httpBackend = _$httpBackend_;
+	 
+	      scope = $rootScope.$new();
+	      ctrl = $controller('AppController', {$scope: scope});
+	    }));
+	
+	    it('should create "Hello" model with "Hello, World!"', function () { 
+	    	expect(scope.Hello).toBe("Hello, World!");
+    	});
+ 	});
+ 	
+ 	describe('BlogController', function(){
+  		var scope, ctrl, $httpBackend;
+  	
+  		beforeEach(module('MyApp.blog'));
+  	
+	  	beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+	      $httpBackend = _$httpBackend_;
+	 
+	      scope = $rootScope.$new();
+	      ctrl = $controller('BlogController', {$scope: scope});
+	    }));
+	
+	    it('should create "Hello" model with "This is my Blog"', function () { 
+	    	expect(scope.Title).toBe("This is my Blog");
+    	});
+ 	});
+ 	
+});
